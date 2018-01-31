971d6e80950b03f3f78cb44d53a8842abc6044ca
Vlad Boboshko <user@A26COMP3>
2018-01-31 11:32:11 +0200

    work 2

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+void main()
+{
+	srand(time(0))
+	int a;
+	cout << "Enter number: ";
+	cin >> a;
+	if (a > 0)
+	{
+		cout << "Good number" << endl;
+	}
+	else
+	{
+		cout << "Bad number" << endl;
+	}
+	int g[5][4];
+	for (int  i = 0; i <5; i++)
+	{
+
+		for (int j = 0; j < 4; j++)
+		{
+			g[i][j] = rand() % 20 + 1;
+		}
+	}
+
+}
